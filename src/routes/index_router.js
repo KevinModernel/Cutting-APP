229b0f46e342523be4dc798bd7ea39417bb46f8e
KevinModernel <modernelkevin96@gmail.com>
2023-01-25 20:51:39 -0300

    Add router to create goal and fix delete route

--- src/routes/index_router.js
+++ src/routes/index_router.js
@@ -1,16 +1,16 @@
 import express from 'express'
 const router = express.Router();
-import {show_landing, create_journey, show_journey, create_goal, store_measure, edit_day, delete_day} from '../controllers/index_controllers.js'
+import {show_landing, create_journey, show_createGoal, show_journey, create_goal, store_measure, edit_day, delete_day} from '../controllers/index_controllers.js'
 
 // Routes "/"
 router.get('/', show_landing);
 router.post('/newjourney', create_journey);
+router.get('/goal/:id', show_createGoal)
 router.get('/journeys/:id', show_journey);
 router.post('/newgoal', create_goal)
 router.post('/newmeasure', store_measure)
 
 // Route PUT not accessible by now
-router.put('/editday/:id', edit_day) // El problema es que tendria que volver a hacer el POST para recalcular los otros campos de la table DailyProgress. Si modularizar bien el "store_measure" podrias volver a llamar a los servicios y editarlo dentro del "edit_day" y chau.
-router.post('/deleteday/:id', delete_day)
+router.post('/deleteday/:journeyid/:id', delete_day)
 
 export default router
