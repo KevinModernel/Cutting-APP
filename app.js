f6b7727faff7c7c090d85631961e7976a604c21a
KevinModernel <modernelkevin96@gmail.com>
2023-01-27 17:43:53 -0300

    Format and change dir of views
    Move views to public folder and minor format changes to improve readability.

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 import express from 'express'
-import index_router from './src/routes/index_router.js'
 import bodyParser from 'body-parser'
 import methodOverride from 'method-override'
+import index_router from './src/routes/index_router.js'
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 app.use('/', index_router);
+
 // View engine
-app.set('views', './src/views');
+app.set('views', './public/views');
 app.set('view engine', 'pug');
 
 export default app
