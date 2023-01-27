de2e99b7849641f6f34bf1a860d8d1b8ff41cfb2
KevinModernel <modernelkevin96@gmail.com>
2023-01-27 18:01:19 -0300

    Minor format changes
    Format changes to improve readability.

--- src/routes/index_router.js
+++ src/routes/index_router.js
@@ -1,13 +1,19 @@
 import express from 'express'
+import {show_landing, 
+	create_journey, 
+	show_createGoal, 
+	show_journey, 
+	create_goal, 
+	store_measure, 
+	delete_day} from '../controllers/index_controllers.js'
 const router = express.Router();
-import {show_landing, create_journey, show_createGoal, show_journey, create_goal, store_measure, delete_day} from '../controllers/index_controllers.js'
 
-// Routes "/"
+// Routes "/" path.
 router.get('/', show_landing);
 router.post('/newjourney', create_journey);
 router.get('/goal/:id', show_createGoal)
-router.get('/journeys/:id', show_journey);
 router.post('/newgoal', create_goal)
+router.get('/journeys/:id', show_journey);
 router.post('/newmeasure', store_measure)
 router.post('/deleteday/:journeyid/:id', delete_day)
 
