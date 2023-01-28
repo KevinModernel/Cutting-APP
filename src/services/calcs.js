8b8b072d926c48266726990b4a88b8347b559896
KevinModernel <modernelkevin96@gmail.com>
2023-01-27 21:39:08 -0300

    Refactor working code
    Code was working but it was messy. Refactored following Single responsibility principle, readability and maintainability improved. (Manual tested Ok).

--- src/services/calcs.js
+++ src/services/calcs.js
@@ -1,59 +1,45 @@
-// Separar funcion "calcs" para cada dato.
-export const days = (startDate, endDate) => { // OK
+export const subtractDays = (startDate, endDate) => {
 	const days = (Date.parse(endDate)- Date.parse(startDate))/(8.64*10**7);
 	return days
 };
 
-export const endBF = (startBodyWeight, startBodyFat, endBW) => {
-	const endBF = startBodyWeight*(startBodyFat/100) - (startBodyWeight - endBW);
-	return endBF
+export const calculateEndBodyFat = (startBodyWeight, startBodyFat, endBodyWeight) => {
+	const endBodyFat = startBodyWeight*(startBodyFat/100) - (startBodyWeight - endBodyWeight);
+	return endBodyFat
 };
 
-export const endBFPercentage = (endBW, endBF) => {
-	const endBFPercentage = Math.floor((endBF / endBW) * 100);
-	return endBFPercentage
+export const calculateEndBodyFatPercentage = (endBodyWeight, endBodyFat) => {
+	const endBodyFatPercentage = Math.floor((endBodyFat / endBodyWeight) * 100);
+	return endBodyFatPercentage
 };
 
-export const deltaWeight = (startBodyWeight, endBW) => {
-	const deltaWeight = startBodyWeight - endBW;
+export const subtractWeights = (startBodyWeight, endBodyWeight) => {
+	const deltaWeight = startBodyWeight - endBodyWeight;
 	return deltaWeight
 };
 
-export const dailyLoss = (deltaWeight, days) => {
+export const calculateDailyLoss = (deltaWeight, days) => {
 	const dailyLoss = Math.round( (deltaWeight/days)*1000 ) / 1000; 
 	return dailyLoss
 };
 
-export const weeklyLoss = (dailyLoss) => {
+export const calculateWeeklyLoss = (dailyLoss) => {
 	const weeklyLoss = Math.round( (dailyLoss*7)*1000 ) / 1000;
 	return weeklyLoss
 };
 
-export const weeklyDeficit = (weeklyLoss, ) => {
+export const calculateWeeklyDeficit = (weeklyLoss) => {
 	const fatCalories = 5500;
 	const weeklyDeficit = Math.floor(weeklyLoss*fatCalories);
 	return 	weeklyDeficit
 };
 
-export const dailyVariation = (dailyProgress, weight) => {
-	const dayBefore = dailyProgress.slice(-1);
-	if (dayBefore == false) { // Primera iteracion dayBefore == [] == false
-		return 0
-	}
-	const weightDayBefore = dayBefore[0].weight;
-	const dailyVariation = Math.round( (weight - weightDayBefore)*1000 ) / 1000;
-
+export const calculateDailyVariation = (weightDayBefore, weightCurrentDay) => {
+	const dailyVariation = Math.round( (weightCurrentDay - weightDayBefore)*1000 ) / 1000;
 	return dailyVariation
 };
 
-export const goalDailyWeight = (journey, goal, days, dailyProgress) => {
-	const initialWeight = journey.bodyWeight;
-	const deltaW = deltaWeight(journey.bodyWeight, goal.bw);
-	const dailyL = dailyLoss(deltaW, days)
-	const quantityDays = dailyProgress.length;
-	if (quantityDays == false) { // Primera iteracion quantityDays == 0 == false
-		return journey.bodyWeight
-	}
-	const goalDailyWeight = Math.round( (initialWeight - dailyL*quantityDays)*100) / 100;
-	return goalDailyWeight
+export const CalculateDailyGoalWeight = (startBodyWeight, dailyLoss, daysPassed) => {
+	const dailyGoalWeight = Math.round( (startBodyWeight - dailyLoss*daysPassed)*100) / 100;
+	return dailyGoalWeight
 };
