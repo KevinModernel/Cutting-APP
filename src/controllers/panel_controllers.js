e389255ff4c156f93cf683554cebb4c86c49a9f6
KevinModernel <modernelkevin96@gmail.com>
2023-01-27 23:33:33 -0300

    Delete unnecesarry code and refactor show journey
    Refactor function since changes in utils. Improve code readability and maintainability.
    View updated to this changes.
    Manual testing Ok.

--- src/controllers/panel_controllers.js
+++ src/controllers/panel_controllers.js
@@ -1,58 +1,55 @@
 import { Journey } from '../models/Journey.js'
 import { Goal } from '../models/Goal.js'
 import { DailyProgress } from '../models/DailyProgress.js'
-import { subtractDays, calculateDailyVariation, CalculateDailyGoalWeight, subtractWeights, calculateDailyLoss } from '../services/calcs.js'
+import { getTodayDate, 
+	subtractDays, 
+	calculateDailyVariation, 
+	CalculateDailyGoalWeight, 
+	subtractWeights, 
+	calculateDailyLoss } from '../services/calcs.js'
 
 export const show_journey = async (req, res) => {
+	const { id } = req.params;
+	let dailyProgress = '';
+	let journey;
+	let goal;
+	
+	// Find journey
 	try {
-		let stats = '';
-		let dailyProgress = '';
-		const { id } = req.params;
-		const journey = await Journey.findOne({
+		journey = await Journey.findOne({
 			where: {
 				id
 			}
 		});
-		const goal = await Goal.findOne({
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	};
+	// Find goal of this journey
+	try {
+		goal = await Goal.findOne({
 			where: {
 				journeyId: id
 			}
 		});
-
-		// Gets all dailyProgress
-		try {
-			dailyProgress = await DailyProgress.findAll({
-			where: {
-				journeyId: id
-			}
-			});
-		} catch(e) {
-			console.log(e);
-		}
-
-		// Gets today date and rest journey.endDate to get the days until end of journey
-		let today = new Date();
-		let dd = String(today.getDate()).padStart(2, '0');
-		let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-		let yyyy = today.getFullYear();
-		today = yyyy + '-' + mm + '-' + dd;
-		let daysUntilEnd = ( Date.parse(goal.endDate) - Date.parse(today) ) / (8.64*10**7);
-
-		// Gets daily weight loss goal.
-		let deltaWeight = journey.bodyWeight - goal.bw;
-		let days = ( Date.parse(goal.endDate) - Date.parse(journey.startDate) ) / (8.64*10**7); 
-		let dailyDelta = calculateDailyLoss(deltaWeight, days);
-
-		let aditionalData = {
-			daysUntilEnd,
-			dailyDelta,
-		}
-
-		res.render('journey', { journey, goal, dailyProgress, aditionalData });
-
-	} catch (e) {
-		console.log(e);
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
 	};
+	// Find dailyProgress of this journey
+	try {
+		dailyProgress = await DailyProgress.findAll({
+		where: {
+			journeyId: id
+		}
+		});
+	} catch(error) {
+		return res.status(500).json({ message: error.message });
+	}
+
+	// Get today date and subtract end date, to get the how many days until the end and display it in table.
+	const todayDate = getTodayDate();
+	let daysUntilEnd = subtractDays(todayDate, goal.endDate );
+
+	res.render('journey', { journey, goal, dailyProgress, daysUntilEnd });
 };
 
 export const store_measure = async (req, res) => {
@@ -115,22 +112,6 @@ export const store_measure = async (req, res) => {
 	} catch (e) {
 		console.log(e);
 	}
-};
-
-// Function not used. There is no route for this.
-export const edit_day = async (req, res) => {
-	try {
-		const { id } = req.params; // day ID, not FK. you want to modify each day.
-		const {date, weight} = req.body;
-
-		const day = await DailyProgress.findByPk(id);
-		day.date = date;
-		day.weight = weight;
-
-
-	} catch (error) {
-			return res.status(500).json({ message: error.message });
-	};
 };
 
 export const delete_day = async (req, res) => {
