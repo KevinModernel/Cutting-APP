524a21edb8baefeb88b50134b5abc1a85f6fad50
KevinModernel <modernelkevin96@gmail.com>
2023-01-27 23:30:50 -0300

    Refactor create new goal controller
    Remove the field to enter end body fat %, and now it´s calculated from other inputs.
    Also dailyLoss was added since it´s an important value of the goal.
    (Also some minor changes to improve readability),

--- src/controllers/goal_controllers.js
+++ src/controllers/goal_controllers.js
@@ -1,5 +1,7 @@
 import {Journey} from '../models/Journey.js';
 import {Goal} from '../models/Goal.js';
+import { calculateEndBodyFat, calculateEndBodyFatPercentage, subtractDays, calculateDailyLoss } from '../services/calcs.js'
+
 
 export const show_createGoal = async (req, res) => {
 	const { id } = req.params; // Journey id is used to assign foreign key to new goal.
@@ -7,12 +9,36 @@ export const show_createGoal = async (req, res) => {
 };
 
 export const create_goal = async (req, res) => {
-	const {bw, bf, endDate, journeyId} = req.body;
+	const {endBodyWeight, endDate, journeyId} = req.body;
+	let journey;
+	let goal;
+	// Find journey
+	try {
+		journey = await Journey.findOne({
+			where: {
+				id: journeyId,
+			}
+		});
+	} catch (error) {
+		return res.status(500).json({ message: error.message });
+	};	
+
+	// Get daily weight loss goal.
+	let deltaWeight = journey.bodyWeight - endBodyWeight;
+	const days = subtractDays(journey.startDate, endDate);
+	const dailyLoss = calculateDailyLoss(deltaWeight, days);
+
+	// Calculate final body fat %
+	const startBodyWeight = journey.bodyWeight;
+	const startBodyFat = journey.bodyFat;
+	const endBodyFat = calculateEndBodyFat(startBodyWeight, startBodyFat, endBodyWeight);
+	const endBodyFatPercentage = calculateEndBodyFatPercentage(endBodyWeight, endBodyFat);
 
 	try { // Stores new Goal in the DB and redirects to follow up panel.
 		const newGoal = await Goal.create({
-			bw,
-			bf,
+			bw: endBodyWeight,
+			bf: endBodyFatPercentage,
+			dailyLoss,
 			endDate,
 			journeyId,
 		});
